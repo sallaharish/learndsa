6328df2b576519ff790350f7b51c61edf23b9054
Salla Harish <sallaharish040@gmail.com>
2026-05-30 13:21:15 +0530

    2/1000

---
+++ 2_warmup/5_loops_2.js
@@ -0,0 +1,17 @@
+// write the function to serach a aeleemt in thte arry and retuen the index of it ..if the element is not present return -1
+
+
+function searchElement(arr,val) {
+    let a = arr.length
+    for (let i = 0; i < a; i++){
+        if (arr[i] == val) {
+            return i
+        }
+    }
+    
+        return -1
+    
+}
+
+console.log(searchElement([1, 2, 2, 3, 4, 3, 2], 3
+))
