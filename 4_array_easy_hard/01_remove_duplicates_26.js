d8e9e6394a0fa3d284f4b237734cee25520eae5b
Salla Harish <sallaharish040@gmail.com>
2026-06-06 12:50:19 +0530

    9/1000

--- 4_array_easy_hard/01_remove_duplicates_26.js
+++ 4_array_easy_hard/01_remove_duplicates_26.js
@@ -1,1 +1,18 @@
 // will be uploaded
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    // to locate the position in array where we need to place the element
+    let x=0;
+    // second pointer for iterating over the entire array
+    for(let i=0;i<=nums.length;i++){
+        if(nums[i+1]>nums[i]){
+            x+=1
+            nums[x]=nums[i+1]
+     
+        }
+    }
+    return x+1
+};
